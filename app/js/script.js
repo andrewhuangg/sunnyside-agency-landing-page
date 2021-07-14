517d375370fb1d342aea9b0c1664293803d4b0db
Andrew <drewhuangg@gmail.com>
2021-07-14 14:39:36 -0700

    mobile menu and animation added

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,22 @@
-console.log('hello world!');
+const hamburgerBtn = document.querySelector('#hamburger-btn');
+const header = document.querySelector('.header');
+const fadeElements = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+
+hamburgerBtn.addEventListener('click', function () {
+  if (header.classList.contains('open')) {
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElements.forEach((ele) => {
+      ele.classList.remove('fade-in');
+      ele.classList.add('fade-out');
+    });
+  } else {
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElements.forEach((ele) => {
+      ele.classList.remove('fade-out');
+      ele.classList.add('fade-in');
+    });
+  }
+});
